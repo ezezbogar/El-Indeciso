adc1b2672f8499bfa5386b692caad84957c09b96
AnaGutson <83771013+AnaGutson@users.noreply.github.com>
2021-06-19 16:46:12 -0300

    Menu Layout
    Project Update with a valid Menu Layout

--- app/src/main/java/com/example/el_indeciso/MainActivity.kt
+++ app/src/main/java/com/example/el_indeciso/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.el_indeciso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.el_indeciso.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Binding object instance with access to the views in the activity_main.xml layout
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Inflate the layout XML file and return a binding object instance
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Set the content view of the Activity to be the root view of the layout
+        setContentView(binding.root)
     }
 }
