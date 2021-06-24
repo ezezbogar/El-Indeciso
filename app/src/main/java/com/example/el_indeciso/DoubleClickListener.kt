86d19baa418c3a7355e90b50d4de4b8a1d3ad58a
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2021-06-24 01:46:46 -0300

    Agrego listener de doble click

---
+++ app/src/main/java/com/example/el_indeciso/DoubleClickListener.kt
@@ -0,0 +1,18 @@
+package com.example.frasesitas.model
+
+import android.view.View
+
+abstract class DoubleClickListener : View.OnClickListener {
+    private var last_click_time: Long = 0
+    private val DOUBLE_CLICK_TIME_DELTA: Long = 300 //milliseconds
+
+    override fun onClick(v: View) {
+        val click_time = System.currentTimeMillis()
+        if (click_time - last_click_time < DOUBLE_CLICK_TIME_DELTA) {
+            onDoubleClick(v)
+            last_click_time = 0
+        }
+        last_click_time = click_time
+    }
+    abstract fun onDoubleClick(v: View)
+}
