545b498214eae3d6c2bb7ab73afd7162f64ad9c3
RiedelNicolas <nariedel99@gmail.com>
2021-06-30 01:08:25 -0300

    agrego mensajes a firebase desde la app

--- app/src/main/java/com/example/el_indeciso/MainActivity.kt
+++ app/src/main/java/com/example/el_indeciso/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.el_indeciso
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.el_indeciso.databinding.ActivityMainBinding
+import com.example.firebaseplayground.Move
+import com.example.firebaseplayground.MovesMessenger
 
 class MainActivity : AppCompatActivity() {
     // Binding object instance with access to the views in the activity_main.xml layout
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         // Set the content view of the Activity to be the root view of the layout
         setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        var mensajero = MovesMessenger("DesdeElRepo")
+        mensajero.addMove(Move(1,1))
+        mensajero.addMove(Move(1,2))
     }
 }
