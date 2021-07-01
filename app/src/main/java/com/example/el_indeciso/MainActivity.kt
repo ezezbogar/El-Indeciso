811274c6eb0f43ba97868c41314370f03babb324
AnaGutson <83771013+AnaGutson@users.noreply.github.com>
2021-07-01 20:53:01 -0300

    Navegación básica entre el menú y la creación de perfil
    Creación básica de una interacción entre el menú y la creación de perfil: realizado con intents y con dos activitys simples

--- app/src/main/java/com/example/el_indeciso/MainActivity.kt
+++ app/src/main/java/com/example/el_indeciso/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.el_indeciso
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.el_indeciso.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,15 +12,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         // Inflate the layout XML file and return a binding object instance
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Set the content view of the Activity to be the root view of the layout
-        setContentView(binding.root)
-        setContentView(R.layout.activity_main)
         var mensajero = MovesMessenger("DesdeElRepo")
-        mensajero.addMove(Move("Niqui",1))
-        mensajero.addMove(Move("Eze",2))
+        mensajero.addMove(Move("Niqui", 1))
+        mensajero.addMove(Move("Eze", 2))
+
+        binding.profileButton.setOnClickListener {
+            val intent = Intent(this, ProfileMenu::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/el_indeciso/ProfileMenu.kt
+++ app/src/main/java/com/example/el_indeciso/ProfileMenu.kt
@@ -1,11 +1,15 @@
 package com.example.el_indeciso
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.el_indeciso.databinding.ActivityMainBinding
+import com.example.el_indeciso.databinding.ProfileMenuBinding
 
-class ProfileMenu: AppCompatActivity() {
+class ProfileMenu : AppCompatActivity() {
+    private lateinit var binding: ProfileMenuBinding
 
     private var back_index = 0
     private var face_index = 0
@@ -14,79 +18,102 @@ class ProfileMenu: AppCompatActivity() {
 
     var profile_pic: String = "0000"
 
+    companion object {
+        val BACKGROUNDS = listOf(
+            R.drawable.back_0, R.drawable.back_1, R.drawable.back_2,
+            R.drawable.back_3, R.drawable.back_4, R.drawable.back_5,
+            R.drawable.back_6, R.drawable.back_7, R.drawable.back_8,
+            R.drawable.back_9,
+        )
+        val FACES = listOf(
+            R.drawable.face_0, R.drawable.face_1, R.drawable.face_2,
+            R.drawable.face_3, R.drawable.face_4, R.drawable.face_5,
+            R.drawable.face_6,
+        )
+        val OUTFITS = listOf(
+            R.drawable.outfit_0, R.drawable.outfit_1,
+            R.drawable.outfit_2, R.drawable.outfit_3,
+        )
+        val HEADS = listOf(
+            R.drawable.head_0, R.drawable.head_1, R.drawable.head_2,
+            R.drawable.head_3, R.drawable.head_4, R.drawable.head_5,
+            R.drawable.head_6,
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile_menu)
 
-        val head: ImageView = findViewById(R.id.head)
-        val face: ImageView = findViewById(R.id.face)
-        val outfit: ImageView = findViewById(R.id.outfit)
-        val back: ImageView = findViewById(R.id.back)
+        // Inflate the layout XML file and return a binding object instance
+        binding = ProfileMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.next_head).setOnClickListener{
-            head_index++
-            if (head_index >= HEADS.size) head_index = 0
-            head.setImageResource(HEADS[head_index])
+        val head: ImageView = binding.headProfileMenu
+        val face: ImageView = binding.faceProfileMenu
+        val outfit: ImageView = binding.outfitProfileMenu
+        val back: ImageView = binding.backProfileMenu
+
+        binding.nextHead.setOnClickListener {
+            head_index = nextButtonClicked(head_index, HEADS, head)
         }
-        findViewById<Button>(R.id.prev_head).setOnClickListener{
-            head_index--
-            if (head_index < 0) head_index = HEADS.size - 1
-            head.setImageResource(HEADS[head_index])
+        binding.prevHead.setOnClickListener {
+            head_index = prevButtonClicked(head_index, HEADS, head)
         }
-        findViewById<Button>(R.id.next_face).setOnClickListener{
-            face_index++
-            if (face_index >= FACES.size) face_index = 0
-            face.setImageResource(FACES[face_index])
+        binding.nextFace.setOnClickListener {
+            face_index = nextButtonClicked(face_index, FACES, face)
         }
-        findViewById<Button>(R.id.prev_face).setOnClickListener{
-            face_index--
-            if (face_index < 0) face_index = FACES.size - 1
-            face.setImageResource(FACES[face_index])
+        binding.prevFace.setOnClickListener {
+            face_index = prevButtonClicked(face_index, FACES, face)
         }
-        findViewById<Button>(R.id.next_outfit).setOnClickListener{
-            outfit_index++
-            if (outfit_index >= OUTFITS.size) outfit_index = 0
-            outfit.setImageResource(OUTFITS[outfit_index])
+        binding.nextOutfit.setOnClickListener {
+            outfit_index = nextButtonClicked(outfit_index, OUTFITS, outfit)
         }
-        findViewById<Button>(R.id.prev_outfit).setOnClickListener{
-            outfit_index--
-            if (outfit_index < 0) outfit_index = OUTFITS.size - 1
-            outfit.setImageResource(OUTFITS[outfit_index])
+        binding.prevOutfit.setOnClickListener {
+            outfit_index = prevButtonClicked(outfit_index, OUTFITS, outfit)
         }
-        findViewById<Button>(R.id.next_back).setOnClickListener{
-            back_index++
-            if (back_index >= BACKGROUNDS.size) back_index = 0
-            back.setImageResource(BACKGROUNDS[back_index])
+        binding.nextBack.setOnClickListener {
+            back_index = nextButtonClicked(back_index, BACKGROUNDS, back)
         }
-        findViewById<Button>(R.id.prev_back).setOnClickListener{
-            back_index--
-            if (back_index < 0) back_index = BACKGROUNDS.size - 1
-            back.setImageResource(BACKGROUNDS[back_index])
+        binding.prevBack.setOnClickListener {
+            back_index = prevButtonClicked(back_index, BACKGROUNDS, back)
         }
-        findViewById<Button>(R.id.save_button).setOnClickListener {
+        binding.saveButtonProfileMenu.setOnClickListener {
             val back_digit = Integer.toHexString(back_index)
             val head_digit = Integer.toHexString(head_index)
             val face_digit = Integer.toHexString(face_index)
             val outfit_digit = Integer.toHexString(outfit_index)
 
             profile_pic = "${back_digit}${head_digit}${face_digit}${outfit_digit}"
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
-
     }
 
+    private fun nextButtonClicked(
+        _avatarPartIndex: Int,
+        listOfAvatarPart: List<Int>,
+        imageView: ImageView
+    )
+            : Int {
+        var avatarPartIndex = _avatarPartIndex
+        avatarPartIndex++
+        if (avatarPartIndex >= listOfAvatarPart.size) avatarPartIndex = 0
+        imageView.setImageResource(listOfAvatarPart[avatarPartIndex])
 
-    companion object {
-        val BACKGROUNDS = listOf(R.drawable.back_0,R.drawable.back_1,R.drawable.back_2,
-            R.drawable.back_3,R.drawable.back_4,R.drawable.back_5,
-            R.drawable.back_6,R.drawable.back_7,R.drawable.back_8,
-            R.drawable.back_9,)
-        val FACES = listOf(R.drawable.face_0,R.drawable.face_1,R.drawable.face_2,
-            R.drawable.face_3,R.drawable.face_4,R.drawable.face_5,
-            R.drawable.face_6,)
-        val OUTFITS = listOf(R.drawable.outfit_0,R.drawable.outfit_1,
-            R.drawable.outfit_2,R.drawable.outfit_3,)
-        val HEADS = listOf(R.drawable.head_0,R.drawable.head_1,R.drawable.head_2,
-            R.drawable.head_3,R.drawable.head_4,R.drawable.head_5,
-            R.drawable.head_6,)
+        return avatarPartIndex
+    }
+
+    private fun prevButtonClicked(
+        _avatarPartIndex: Int,
+        listOfAvatarPart: List<Int>,
+        imageView: ImageView
+    )
+            : Int {
+        var avatarPartIndex = _avatarPartIndex
+        avatarPartIndex--
+        if (avatarPartIndex < 0) avatarPartIndex = listOfAvatarPart.size - 1
+        imageView.setImageResource(listOfAvatarPart[avatarPartIndex])
+
+        return avatarPartIndex
     }
 }
